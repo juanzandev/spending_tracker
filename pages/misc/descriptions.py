bdabdec92e19c0b6cbd24972bafe79ba43fe2d36
Juanzan <hunterleaguecanal@gmail.com>
2024-11-06 23:06:39 -0600

    updated descriptions.py

--- pages/misc/descriptions.py
+++ pages/misc/descriptions.py
@@ -1,4 +1,13 @@
-overallDescription = """[Overall Descriptions]"""
+overallDescription = """Meet the Syntax Society Team!"""
+
+# Nolan is a first year undergraduate student from Kentucky, focusing in software and hardware engineering.
+
+# J from Argentina: J is a computer science student from Buenos Aires with interests in software development and cybersecurity, who loves tango, football, and Argentine cuisine.
+
+# D from Chicago: D is a computer science student focused on data science and cloud computing, who enjoys Chicago's cultural scene, live music, and deep-dish pizza."""
+
+# (do this later)
+
 
 juanDescription = """
 <h3>Juan Zanguitu</h3>
@@ -22,6 +31,22 @@ nolanDescription = """
 <h6>First Year CS Student</h6>
 
 nlawrence1@hawk.iit.edu
+"""
 
-Description
+
+def pieChartDescription(totalSpending, totalBudget, moneyLeft):
+    return f"""
+<h2>Welcome to Spending Analyzer!</h2>
+
+<h4>Here you can see a summary of your transactions over the past month, and how they effect your overall budget.</h4>
+
+
+Your budget is \${totalBudget}.
+
+You spent \${totalSpending}.
+
+You currently have \${moneyLeft} dollars left to spend this month.
+
+
+<h5>Congratulations! You are on track to increasing your spending score this month! </h5>
 """
